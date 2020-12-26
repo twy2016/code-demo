a1ce5b06b3c33f0641e4e342a3798fc7d10097b6
gongpeng <whgp1994@163.com>
2020-12-26 13:34:27 +0800

    添加java8新特性代码演示项目

---
+++ feature-java/src/main/java/com/twy/featurejava/FeatureJavaApplication.java
@@ -0,0 +1,17 @@
+package com.twy.featurejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gongpeng
+ * @date 2020/12/26 13:31
+ */
+@SpringBootApplication
+public class FeatureJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureJavaApplication.class, args);
+	}
+
+}
