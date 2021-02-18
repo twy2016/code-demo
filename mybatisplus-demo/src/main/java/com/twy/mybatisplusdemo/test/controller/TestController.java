c299b2d30a0548ffcbb95cf01896f306438d2e90
twy <whgp1994@163.com>
2021-02-18 23:45:25 +0800

    feat: 添加日期传参测试

--- mybatisplus-demo/src/main/java/com/twy/mybatisplusdemo/test/controller/TestController.java
+++ mybatisplus-demo/src/main/java/com/twy/mybatisplusdemo/test/controller/TestController.java
@@ -4,10 +4,10 @@ package com.twy.mybatisplusdemo.test.controller;
 import com.twy.mybatisplusdemo.test.entity.Test;
 import com.twy.mybatisplusdemo.test.service.TestService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -24,8 +24,21 @@ public class TestController {
 
     private final TestService testService;
 
-    @PostMapping
-    public boolean save(@RequestBody Test test) {
+    @PostMapping("save1")
+    public boolean save1(@RequestBody Test test) {
+        return testService.save(test);
+    }
+
+    @PostMapping("save2")
+    public boolean save2(@RequestParam(required = false) String name, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createTime) {
+        Test test = new Test();
+        test.setName(name);
+        test.setCreateTime(createTime);
+        return testService.save(test);
+    }
+
+    @GetMapping("save3")
+    public boolean save3(Test test) {
         return testService.save(test);
     }
 }
--- mybatisplus-demo/src/main/java/com/twy/mybatisplusdemo/test/entity/Test.java
+++ mybatisplus-demo/src/main/java/com/twy/mybatisplusdemo/test/entity/Test.java
@@ -3,8 +3,10 @@ package com.twy.mybatisplusdemo.test.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -29,6 +31,8 @@ public class Test implements Serializable {
 
     private String name;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
 }
